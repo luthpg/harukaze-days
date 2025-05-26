0c60355bb6dbb8dfeba2f17913733d376dd4cea8
luth <88308177+luthpg@users.noreply.github.com>
2025-05-26 22:56:34 +0900

    add ogp

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,3 +1,4 @@
+import { env } from '@/env';
 import { supabase } from '@/lib/db';
 import { sessionAtom } from '@/stores/auth';
 import { Outlet, createRootRoute } from '@tanstack/react-router';
@@ -5,7 +6,70 @@ import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import { useAtom } from 'jotai';
 import { useEffect } from 'react';
 
+const title = 'はるかぜ';
+const description = 'シンプルな日付記録アプリ';
+const xAuther = '@luthpg';
+
 export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        title: title,
+      },
+      {
+        name: 'description',
+        content: description,
+      },
+      {
+        name: 'twitter:card',
+        content: 'summary_large_image',
+      },
+      {
+        name: 'twitter:site',
+        content: xAuther,
+      },
+      {
+        name: 'twitter:creator',
+        content: xAuther,
+      },
+      {
+        name: 'twitter:description',
+        content: description,
+      },
+      {
+        name: 'twitter:title',
+        content: title,
+      },
+      {
+        name: 'twitter:image',
+        content: `${env.VITE_APP_URL}/logo192.png`,
+      },
+      {
+        property: 'og:image',
+        content: `${env.VITE_APP_URL}/logo192.png`,
+      },
+      {
+        property: 'og:title',
+        content: title,
+      },
+      {
+        property: 'og:description',
+        content: description,
+      },
+      {
+        property: 'og:url',
+        content: env.VITE_APP_URL,
+      },
+      {
+        property: 'og:type',
+        content: 'website',
+      },
+      {
+        property: 'og:site_name',
+        content: title,
+      },
+    ],
+  }),
   component: () => {
     const [, setSession] = useAtom(sessionAtom);
 
