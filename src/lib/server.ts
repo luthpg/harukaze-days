74389fe2a38efbdc48c614e7d8607c3ae599e5ba
luth <88308177+luthpg@users.noreply.github.com>
2025-05-26 16:01:45 +0900

    change file name

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -1,4 +1,4 @@
-import type { AppType } from 'api/index';
+import type { AppType } from 'api/[[...routes]]';
 import { hc } from 'hono/client';
 
 export const honoClient = hc<AppType>('http://localhost:3001/api');
